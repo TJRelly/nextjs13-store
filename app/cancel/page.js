221fefab12ca3b43d683e35af66a05f3b77129fe
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-09-15 11:31:55 -0400

    updated cancel page

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -3,7 +3,7 @@ import { BiArrowBack } from "react-icons/bi"
 
 export default function CancelPage() {
     return (
-        <div>
+        <div className="p-4 grid gap-4">
             {"We're here for you when you need us!"}
             <Link href={"/"}>
         <div className="flex items-center gap-2 border border-green-700 rounded p-2 w-fit text-white bg-green-700 hover:opacity-60">
